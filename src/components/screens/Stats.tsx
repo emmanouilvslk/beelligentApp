b831295ba97661cd6706c93db5756a5e151f756a
emmanouilvslk <emmanouilvslk@gmail.com>
2021-09-16 21:29:17 +0300

    temp values in stats  chart kit

--- src/components/screens/Stats.tsx
+++ src/components/screens/Stats.tsx
@@ -4,40 +4,9 @@ import { scale, padding } from "styles/scaling";
 import { LineChart } from "react-native-chart-kit";
 
 type StatsProps = {};
-type Measurementitem = {
-    weight: number;
-    temperature: number;
-    humidity: number;
-    time: Date;
-};
-const contentPadding = padding(50, 25, 5, 25);
 
 const Stats: React.FC<StatsProps> = ({}) => {
-    const [isLoading, setLoading] = useState<boolean>(true);
-    const [data1, setData] = useState([]);
-    const [temperature, setTemperature] = useState([]);
-
-    const getMeasurements = async () => {
-        try {
-            const response = await fetch("http://theater1.dyndns.org/measurements");
-            const json = await response.json();
-            setData(json);
-            getTemperature();
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
-    const getTemperature = () => {
-        const temperatureArray: any = data1.map((item: any) => {
-            return item.measurement.temperature;
-        });
-        setTemperature(temperatureArray);
-    };
-
-    useEffect(() => {
-        getMeasurements();
-    }, []);
+    const tempArray = [30, 31.3, 26.5, 27.5, 28, 29, 23.5];
 
     return (
         <View style={{ flex: 1, position: "relative" }}>
@@ -46,44 +15,45 @@ const Stats: React.FC<StatsProps> = ({}) => {
                     <View style={{ justifyContent: "center", alignItems: "center", paddingTop: scale(15) }}>
                         <Text> Temperature </Text>
                     </View>
-
-                    <LineChart
-                        data={{
-                            labels: [],
-                            datasets: [
-                                {
-                                    data: temperature,
+                    <View>
+                        <LineChart
+                            data={{
+                                labels: ["1st", "10th", "20th", "30th"],
+                                datasets: [
+                                    {
+                                        data: tempArray,
+                                    },
+                                ],
+                            }}
+                            width={Dimensions.get("window").width} // from react-native
+                            height={Dimensions.get("window").height / 4}
+                            yAxisLabel=""
+                            yAxisSuffix="°C"
+                            yAxisInterval={1} // optional, defaults to 1
+                            chartConfig={{
+                                backgroundColor: "white",
+                                backgroundGradientFrom: "rgb(254,229,37)",
+                                backgroundGradientTo: "gray",
+                                decimalPlaces: 2, // optional, defaults to 2dp
+                                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                                style: {
+                                    borderRadius: 16,
                                 },
-                            ],
-                        }}
-                        width={Dimensions.get("window").width} // from react-native
-                        height={Dimensions.get("window").height / 4}
-                        yAxisLabel=""
-                        yAxisSuffix="°C"
-                        yAxisInterval={1} // optional, defaults to 1
-                        chartConfig={{
-                            backgroundColor: "white",
-                            backgroundGradientFrom: "rgb(254,229,37)",
-                            backgroundGradientTo: "gray",
-                            decimalPlaces: 2, // optional, defaults to 2dp
-                            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                            style: {
-                                borderRadius: 16,
-                            },
-                            propsForDots: {
-                                r: "6",
-                                strokeWidth: "2",
-                                stroke: "gray",
-                            },
-                        }}
-                        bezier
-                        style={{
-                            borderRadius: scale(16),
-                            marginRight: scale(35),
-                            padding: scale(25),
-                        }}
-                    />
+                                propsForDots: {
+                                    r: "6",
+                                    strokeWidth: "2",
+                                    stroke: "gray",
+                                },
+                            }}
+                            bezier
+                            style={{
+                                borderRadius: scale(16),
+                                marginRight: scale(35),
+                                padding: scale(25),
+                            }}
+                        />
+                    </View>
                 </View>
             </View>
         </View>
